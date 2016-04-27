6c86f29c1eb9c534b9c983839b6ad0cb2d3f4e52
Alastair Rayner <alli0@me.com>
2016-04-27 10:52:16 +0100

    Now checks x and y values of the cell for oxygen spawn
    forgot to check he y value aswell as the x value for spawning oxygen in
    a cell

--- our code/SDL_project/Oxygen.cpp
+++ our code/SDL_project/Oxygen.cpp
@@ -19,31 +19,33 @@ void Oxygen::spawnOxygen(int x, int y, int cell_X, int cell_Y, Cell cell)
 {
 	Grid grid;
 	int oxygen_Level = cell.get_Oxygen_level();
-	if (x >= cell.getX() - grid.getCellSize() && x <= cell_X + grid.getCellSize())
+	if (x >= cell.getX() - grid.getCellSize() && x <= cell.getX() + grid.getCellSize())
 	{
-		if (cell.isRoom)
+		if (y >= cell.getY() - grid.getCellSize() && y <= cell.getY() + grid.getCellSize())
 		{
-			if (cell.get_Oxygen_level() >= 100)
+			if (cell.isRoom)
 			{
-				//Oyxgen in cell is full
-				Set_Oxygen_Level(100);
-				cell.isOxygenated = true;
-				cell.set_Oxygen_level(100);
-				SDL_Log("Oxygen full");
-				//update cell sprite
+				if (cell.get_Oxygen_level() >= 100)
+				{
+					//Oyxgen in cell is full
+					Set_Oxygen_Level(100);
+					cell.isOxygenated = true;
+					cell.set_Oxygen_level(100);
+					SDL_Log("Oxygen full");
+					//update cell sprite
 
+				}
+
+				else
+				{
+					cell.isOxygenated = false;
+					oxygen_Level++;
+					Set_Oxygen_Level(oxygen_Level);
+					cell.set_Oxygen_level(oxygen_Level);
+					SDL_Log("Spawning oxygen %i", cell.get_Oxygen_level());
+
+				}
 			}
-
-			else
-			{
-				cell.isOxygenated = false;
-				oxygen_Level++;
-				Set_Oxygen_Level(oxygen_Level);
-				cell.set_Oxygen_level(oxygen_Level);
-				SDL_Log("Spawning oxygen %i", cell.get_Oxygen_level());
-
-			}
-
 		}
 	}
 }
