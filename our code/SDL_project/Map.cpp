a59a84f504a84170ae84ceffceb765057020b716
Warwick New(wn182882) <mistercheeez@hotmail.co.uk>
2016-04-27 11:19:19 +0100

    Got rid of old commented out code

--- our code/SDL_project/Map.cpp
+++ our code/SDL_project/Map.cpp
@@ -4,62 +4,6 @@
 /*
  TODO:Get the map from file and loads it into the grid system
 */
-/*
-Map::Map(const std::string& filename)
-{
-	std::ifstream mapFile(filename);
-	const char* MAP_FILE = "Resources\\Map\\test_map.txt";
-
-	map = new Map(MAP_FILE);
-	std::vector<std::string> lines;
-	while (!mapFile.eof())
-	{
-		std::string line;
-		std::getline(mapFile, line);
-
-		//set the width to the width of the screen
-		if (line.length() > width)
-		{
-			width = line.length();
-		}
-	}
-
-	//set the height to the ammount of columns in the text file
-	height = lines.size();
-
-	//loop through the rows in the text file
-	for (int x = 0; x < height; x++)
-	{
-		std::string& line = lines[x];
-
-		//loop through the columns in the text file
-		for (int y = 0; y < height; y++)
-		{
-			char Character;
-			if (x < line.length())
-				Character = line[x];
-			else
-				Character = ' ';
-
-			//Sets Cell_Value based on the chars in the text file
-			switch (Character)
-			{
-			case '#':
-				Cell_Value = 1;
-				break;
-			case '|':
-				Cell_Value = 2;
-				break;
-			default:
-				Cell_Value = 0;
-			}
-		}
-	}
-
-	if (map)
-		delete map;
-}
-*/
 
 void Map::LoadMap(std::string filename, Grid room)
 //(filename, grid to load into) loads map from text file into grid
