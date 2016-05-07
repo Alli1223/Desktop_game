9332f0f171862d70678b6d96c482fa2d3c11c197
Maddie <mk186442@falmouth.ac.uk>
2016-05-07 14:58:06 +0100

    Updated from Developemen

--- our code/SDL_project/ISSLocation.cpp
+++ our code/SDL_project/ISSLocation.cpp
@@ -91,11 +91,13 @@ pplx::task<void> ISSLocation::requestJSONValueAsync()
 
 void ISSLocation::update()
 {
-	if (updateTime > previousUpdateTime + 3)
-	{// Currently updates location 
+	if (updateTime > previousUpdateTime + 30)
+	{// Currently updates location
+		previousUpdateTime = updateTime;
 		requestJSONValueAsync().wait();
-		backgroundXPos = ((latitude / 90) * 800) + 1000; //800 = Screen height
-		backgroundYPos = ((longitude / 180) * 800) + 1000; //800 = Screen width
+		backgroundXPos = ((latitude / 90) * windowHeight) + 1000;
+		backgroundYPos = ((longitude / 180) * windowWidth) + 1000;
 
 	}
+	updateTime = updateTime + 1;
 }
