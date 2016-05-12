534d26c39cfd754c4f038a341ece4f45ec464437
Warwick New(wn182882) <mistercheeez@hotmail.co.uk>
2016-05-12 16:14:41 +0100

    all four directions can place a room

--- our code/SDL_project/Map.cpp
+++ our code/SDL_project/Map.cpp
@@ -43,11 +43,7 @@ void Map::LoadMap(std::string filename, Grid room)
 	}
 }
 
-int Map::random(int smallestValue, int largestValue)
-{
-	std::srand(time(nullptr));
-	return (rand() % (largestValue - smallestValue)) + smallestValue;
-}
+
 
 int roundToNearestWhole(double number) 
 {
@@ -68,19 +64,28 @@ bool Map::generateRoom(Grid level, int size, int entranceX, int entranceY, char 
 	double topLeftY;
 	if (direction == 'n')
 	{
-		topLeftX = roundToNearestWhole(entranceX - size / 2);
-		topLeftY = entranceY - size - 1;
+		topLeftX = entranceX - roundToNearestWhole(size / 2);
+		topLeftY = entranceY - size;
 	}
 	else if (direction == 's')
 	{
-		topLeftX = roundToNearestWhole(entranceX - size / 2);
+		topLeftX = entranceX - roundToNearestWhole(size / 2);
 		topLeftY = entranceY + 1;
 	}
+	else if (direction == 'e')
+	{
+		topLeftX = entranceX + 1;
+		topLeftY = entranceY - roundToNearestWhole(size / 2);
+	}
+	else
+	{
+		topLeftX = entranceX - size;
+		topLeftY = entranceY - roundToNearestWhole(size / 2);
+	}
 
-
-		for (int x = topLeftX; x < topLeftX + size; x++)
-		{
-			if (x < 0 || x > level.grid.size())   										    //Detects if the room goes out the level horizontally
+	for (int x = topLeftX; x < topLeftX + size; x++)
+	{
+			if (x < 0 || x > level.grid.size())   									//Detects if the room goes out the level horizontally
 			{
 				return false;
 			}
@@ -91,11 +96,19 @@ bool Map::generateRoom(Grid level, int size, int entranceX, int entranceY, char 
 				{
 					return false;
 				}
-				level.grid[x][y]->isRoom = true;
 				column.push_back(level.grid[x][y]);
 			}
 			room.push_back(column);
 		}
+		
+		for (int x = 0; x < room.size(); x++)										//Gives all the cells in the room it's properties
+		{
+			for (int y = 0; y < room[0].size(); y++)
+			{
+				room[x][y]->isRoom = true;
+			}
+		}
+
 		roomVector.push_back(room);
 		return true;
 }
@@ -103,45 +116,85 @@ bool Map::generateRoom(Grid level, int size, int entranceX, int entranceY, char 
 void Map::generateMap(Grid level)
 {
 	//Clear a starting room(room 0)
-	//generateRoom(level, 5, 2, -1, 's');
+	generateRoom(level, 3, 2, -1, 's');
 
-	generateRoom(level, 5, 2, 6, 'n');
-
-	int direction = 1;// random(0, 4);
+	int size = random(3, 6);
+	int direction = random(0, 4);
 	if (direction == 0)																//north
 	{
 		if (roomVector[0][0][0]->getY() - 1 > 0)
 		{
 			int yOfDoor = roomVector[0][0][0]->getY() - 1;
 			int xStart = roomVector[0][0][0]->getX();
-			int halfRoomSize = roomVector[0].size() / 2;
+			int halfRoomSize = roundToNearestWhole(roomVector[0].size() / 2);
 			int xOfDoor = xStart + halfRoomSize;
 			if (level.grid[xOfDoor][yOfDoor]->isRoom == false)
 			{
-				level.grid[xOfDoor][yOfDoor]->isRoom = true;
-				level.grid[xOfDoor][yOfDoor]->isDoor = true;
+				if (generateRoom(level, size, xOfDoor, yOfDoor, 'n'))
+				{
+					level.grid[xOfDoor][yOfDoor]->isRoom = true;
+					level.grid[xOfDoor][yOfDoor]->isDoor = true;
+				}
 			}
 		}
-
 	}
-	if (direction == 1)																//east
+	else if (direction == 1)														//east
 	{
 		if (roomVector[0][0][0]->getX() + 1 < level.grid.size())
 		{
 			int xSize = roomVector[0].size();
 			int xOfDoor = roomVector[0][0][0]->getX() + xSize;
 			int yStart = roomVector[0][0][0]->getY();
-			int halfRoomSize = roomVector[0][0].size() / 2;
+			int halfRoomSize = roundToNearestWhole(roomVector[0][0].size() / 2);
 			int yOfDoor = yStart + halfRoomSize;
 			if (level.grid[xOfDoor][yOfDoor]->isRoom == false)
 			{
-				level.grid[xOfDoor][yOfDoor]->isRoom = true;
-				level.grid[xOfDoor][yOfDoor]->isDoor = true;
+				if (generateRoom(level, size, xOfDoor, yOfDoor, 'e'))
+				{
+					level.grid[xOfDoor][yOfDoor]->isRoom = true;
+					level.grid[xOfDoor][yOfDoor]->isDoor = true;
+				}
+			}
+		}
+	}
+	else if (direction == 2)														//south
+	{
+		if (roomVector[0][0].size() + roomVector[0][0][0]->getY() + 1 < level.grid[0].size())
+		{
+			int yOfDoor = roomVector[0][0].size() + roomVector[0][0][0]->getY();
+			int xStart = roomVector[0][0][0]->getX();
+			int halfRoomSize = roundToNearestWhole(roomVector[0].size() / 2);
+			int xOfDoor = xStart + halfRoomSize;
+			if (level.grid[xOfDoor][yOfDoor]->isRoom == false)
+			{
+				if (generateRoom(level, size, xOfDoor, yOfDoor, 's'))
+				{
+					level.grid[xOfDoor][yOfDoor]->isRoom = true;
+					level.grid[xOfDoor][yOfDoor]->isDoor = true;
+				}
 			}
 		}
 
 	}
-
+	else																			//west
+	{
+		if (roomVector[0].size() + roomVector[0][0][0]->getX() + 1 < level.grid.size())
+		{
+			int xSize = roomVector[0].size();
+			int xOfDoor = roomVector[0][0][0]->getX() - 1;
+			int yStart = roomVector[0][0][0]->getY();
+			int halfRoomSize = roundToNearestWhole(roomVector[0][0].size() / 2);
+			int yOfDoor = yStart + halfRoomSize;
+			if (level.grid[xOfDoor][yOfDoor]->isRoom == false)
+			{
+				if (generateRoom(level, size, xOfDoor, yOfDoor, 'w'))
+				{
+					level.grid[xOfDoor][yOfDoor]->isRoom = true;
+					level.grid[xOfDoor][yOfDoor]->isDoor = true;
+				}
+			}
+		}
+	}
 }
 
 Map::Map()
