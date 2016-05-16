3fba1ffe59355961a95a74147a677b621ed377b5
Maddie <maddiek19@outlook.com>
2016-05-16 14:12:18 +0100

    Commented character class

--- our code/SDL_project/Character.cpp
+++ our code/SDL_project/Character.cpp
@@ -10,14 +10,15 @@ Character::~Character()
 {
 }
 
+// Checks keyboard input and moves character accordinly
 void Character::moveCharacter(const Uint8* keyboardState)
-{// This function should only be called if WASD is pressed, it then checks to see which one is pressed and moves the character accordingly
-	 
+{	 
+	// If the W key is pressed and the character will move up as long as it won't be moved off screen
 	if (keyboardState[SDL_SCANCODE_W] && getY() - getSpeed() > 0 && isCellARoom(getX(), getY() - getSpeed()))
-	{ // If the W key is pressed and the character won't be moved off screen move the character
+	{ 
 		setY(getY() - getSpeed());
 	}
-	else if (keyboardState[SDL_SCANCODE_S] && getY() + getSpeed() < 800 && isCellARoom(getX(), getY() + getSpeed())) //Will change to pass in screen dimensions
+	else if (keyboardState[SDL_SCANCODE_S] && getY() + getSpeed() < 800 && isCellARoom(getX(), getY() + getSpeed())) 
 	{
 		setY(getY() + getSpeed());
 	}
@@ -31,14 +32,16 @@ void Character::moveCharacter(const Uint8* keyboardState)
 	}
 }
 
+// Gets a random number from between a given range
 int Character::getRandomNumber(int smallestValue, int largestValue)
 {
 	std::srand(time(nullptr));
 	return (rand() % (largestValue - smallestValue)) + smallestValue;
 }
 
+// If the player doesn't move the character it will start to move around the room in randomly selected directions
 void Character::wanderAroundRoom()
-{ // Makes the character move around the room on it's own it the player doesn't direct it for a certain amount of time
+{ 
 	direction = getRandomNumber(0, 4);
 	if (direction == 0 && getY() + getSpeed() < 800 - getSize() && canWanderInRoom(getX(), getY() + getSpeed()))
 	{//Up
@@ -56,28 +59,32 @@ void Character::wanderAroundRoom()
 	{//Left
 		setX(getX() - getSpeed());
 	}
+	//If the character can't move in the current direction it randomly selects a new one
 	else
 	{
 		direction = getRandomNumber(0, 4);
 	}
 }
 
+// Checks to see if a cell is a room
 bool Character::isCellARoom(int x, int y)
-{// Checks to see if a cell is a room
+{
 	int xCell = x / currentRoom->getCellSize();
 	int yCell = y / currentRoom->getCellSize();
 	return currentRoom->grid[xCell][yCell]->isRoom;
 }
 
+// Checks to see if a cell is a room
 bool Character::isCellADoor(int x, int y)
-{// Checks to see if a cell is a room
+{
 	int xCell = x / currentRoom->getCellSize();
 	int yCell = y / currentRoom->getCellSize();
 	return currentRoom->grid[xCell][yCell]->isDoor;
 }
 
+// Character can only wander around a room and can't go through door
 bool Character::canWanderInRoom(int x, int y)
-{ //Character can only wander around a room and can't go through door
+{ 
 	if (isCellARoom(x, y) && !isCellADoor(x, y) )
 	{
 		return true;
@@ -86,8 +93,9 @@ bool Character::canWanderInRoom(int x, int y)
 		return false;
 }
 
+// Gets the oxygen level of the specified cell
 int Character::getOxygenLevel(int x, int y)
-{// Gets the oxygen level of the specified cell
+{
 	int xCell = x / currentRoom->getCellSize();
 	int yCell = y / currentRoom->getCellSize();
 	return currentRoom->grid[xCell][yCell]->oxygenLevel;
