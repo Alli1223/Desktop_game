ce6ebd22946039340b31c044a7fcf21e7e38fe30
Maddie <maddiek19@outlook.com>
2016-05-16 14:29:42 +0100

    Commented CharacterState and IdleState

--- our code/SDL_project/Character.h
+++ our code/SDL_project/Character.h
@@ -22,15 +22,18 @@ public:
 	int setY(int newY) { return y = newY; }
 	int setSpeed(int newSpeed) { return speed = newSpeed; }
 	
+	//Pointer to the grid being used in SpaceGame
+	std::shared_ptr<Grid> currentRoom;
+
 	//Cell checking functions
 	bool isCellARoom(int x, int y);
 	bool isCellADoor(int x, int y);
+	//Checks to see if a cell is a room cell but not a door cell
 	bool canWanderInRoom(int x, int y);
 	int getOxygenLevel(int x, int y);
 
+	// Chooses a random number between a given range
 	int getRandomNumber(int smallestValue, int largestValue);
-	int direction = 1;
-	
 	// Moves the character depending on the player's input
 	void moveCharacter(const Uint8* keyboardState);
 	// If the user doesn't input a move for a given time the character will wander around
@@ -38,14 +41,12 @@ public:
 	
 	//Stores the characters current state 
 	std::shared_ptr<CharacterState> state;
-	//Pointer to the grid being used in SpaceGame, used to check the state of a given cell.
-	std::shared_ptr<Grid> currentRoom; 
-	std::vector<std::shared_ptr<Cell>> getNeighbourCells();
-	std::vector<std::shared_ptr<Cell>> Character::checkNeighbourCells();
-
-	//Character health level, will change to use getters and setters
+	
 	double health = 100;
+	// Whether character is alive or not
 	bool isAlive = true; 
+	// The direction that the character will move in when in the wandering state
+	int direction = 1;
 	// Timer is used to count how long the character has been in the idle state
 	double timer = 0;
 	// The different speed options for use in different states
@@ -53,8 +54,10 @@ public:
 
 private:
 	// Characters X and Y position
-	int x = 50, y = 50; 
-	int size = 50;  //The size of the character sprite when rendered 
-	int speed = 3; //The speed that the character intially moves at
+	int x = 50, y = 50;
+	// The size of the character sprite when rendered 
+	int size = 50;
+	// The speed at which the character is moved
+	int speed = 3; 
 };
 
