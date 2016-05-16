39cf7013f45025c96f2927dc8ea818bb4c95a49a
Maddie <maddiek19@outlook.com>
2016-05-16 13:10:02 +0100

    Health bar is transparent

--- our code/SDL_project/SpaceGame.cpp
+++ our code/SDL_project/SpaceGame.cpp
@@ -143,8 +143,10 @@ void SpaceGame::run()
 		
 		//Renders the health and oxygen bar
 		healthBar.render(renderer, WINDOW_WIDTH , 25, characterOne.health * 10, 25);
+		healthBar.addTransparency(100);
 		healthText.render(renderer, 750, 25, 73, 22);
 		oxygenBar.render(renderer, WINDOW_WIDTH, 50, 1000, 25);
+		oxygenBar.addTransparency(100);
 		oxygenText.render(renderer, 750, 50, 73, 22);
 		
 		SDL_RenderPresent(renderer);
