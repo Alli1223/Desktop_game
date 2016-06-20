3e52b999f446ac8d642f33c8944b0df254f27670
Alastair Rayner <alli0@me.com>
2016-06-21 00:27:54 +0100

    fixed bugs & only spreads with oxygen present

--- SpaceGame/SDL_project/Fire.cpp
+++ SpaceGame/SDL_project/Fire.cpp
@@ -28,24 +28,28 @@ void Fire::spawn(Level grid, int cellX, int cellY)
 
 void Fire::fireSpread(Level grid, int cellX, int cellY)
 {
-	
+
 	if (grid.grid[cellX][cellY]->isOnFire == true)
 	{
 
 		int randomDirectionX = rand() % (-1 - 2) - 1;
 		int randomDirectionY = rand() % (-1 - 2) - 1;
 
-		//Is room and not a door
-		if (grid.grid[cellX + (randomDirectionX)][cellY + (randomDirectionY)]->isRoom && grid.grid[cellX + (randomDirectionX)][cellY + (randomDirectionY)]->isDoor == false)
+
+		//is within the map
+		if (cellX + (randomDirectionX)+1 <= grid.grid.size() && cellX + (randomDirectionX)-1 >= 0 && cellY + (randomDirectionY)+1 <= grid.grid.size() && cellY + (randomDirectionY)-1 >= 0)
 		{
-			//is within the map
-			if (cellX + (randomDirectionX) +1 <= grid.grid.size() && cellX + (randomDirectionX) -1 >= 0 && cellY + (randomDirectionY)+1 <= grid.grid.size() && cellY + (randomDirectionY)-1 >= 0)
+			//Is room and not a door
+			if (grid.grid[cellX + (randomDirectionX)][cellY + (randomDirectionY)]->isRoom && grid.grid[cellX + (randomDirectionX)][cellY + (randomDirectionY)]->isDoor == false)
 			{
-				int randomSpawnChance = rand() % (0 - 50);
-				if (randomSpawnChance <= 1)
+				if (grid.grid[cellX + (randomDirectionX)][cellY + (randomDirectionY)]->getOxygenLevel() > 0)
 				{
-					grid.grid[cellX + (randomDirectionX)][cellY + (randomDirectionY)]->isOnFire = true;
-					//grid.grid[cellX + (randomDirectionX)][cellY + (randomDirectionY)]->oxygenLevel = 0;
+					int randomSpawnChance = rand() % (0 - 50);
+					if (randomSpawnChance <= 1)
+					{
+						grid.grid[cellX + (randomDirectionX)][cellY + (randomDirectionY)]->isOnFire = true;
+						grid.grid[cellX + (randomDirectionX)][cellY + (randomDirectionY)]->oxygenLevel = 0;
+					}
 				}
 			}
 		}
