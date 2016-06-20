145f624ed88b741b51db87670ef73e0430908116
Alastair Rayner <alli0@me.com>
2016-06-21 00:19:31 +0100

    fire now spreads properly through rooms
    further work needs to be done to reduce the crashes

--- SpaceGame/SDL_project/Fire.h
+++ SpaceGame/SDL_project/Fire.h
@@ -5,12 +5,14 @@ class Fire
 public:
 	Fire();
 	~Fire();
-
+	//! Determines how the fire will spread
 	void fireSpread(Level grid, int cellX, int cellY);
 
+	//! Spawns the fire at level start
 	void spawn(Level grid, int cellX, int cellY);
 
 private:
 	int fireSpawnChance = 60;
+	int spawnRate = 10;
 };
 
