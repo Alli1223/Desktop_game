2ca83243dc9d06fa32e7a17cdaf9d60bced1a145
Maddie <mk186442@falmouth.ac.uk>
2016-06-29 16:47:23 +0100

    Commented GameSettings

--- SpaceGame/SDL_project/GameSettings.h
+++ SpaceGame/SDL_project/GameSettings.h
@@ -5,6 +5,7 @@ public:
 	GameSettings();
 	~GameSettings();
 
+	//! int that will be used to control movement speed
 	const int FRAME_RATE = 60;
 
 	int WINDOW_WIDTH = 1000;
@@ -12,11 +13,13 @@ public:
 
 	bool fullscreen = false;
 
+	// Will be WASD/default movement
 	int DEAFULT_UP;
 	int DEAFULT_DOWN;
 	int DEFAULT_LEFT;
 	int DEFAULT_RIGHT;
 
+	//Alternate movement keys will be customisable
 	int ALTERNATE_UP;
 	int ALTERNATE_DOWN;
 	int ALTERNATE_LEFT;
